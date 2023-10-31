07852e5f68ff0049287cd96310f5ed11da429614
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-10-31 20:30:47 +0100

    Paginacja

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,28 +3,29 @@
 @section('content')
     <div class="container">
         <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Email</th>
-            <th scope="col">Imię</th>
-            <th scope="col">Nazwisko</th>
-            <th scope="col">Numer telefonu</th>
-            <th scope="col">Akcje</th>
-        </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                <tr>
-                    <th scope="row">{{ $user->id }}</th>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->surname }}</td>
-                    <td>{{ $user->phone_number }}</td>
-                    <td></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Email</th>
+                <th scope="col">Imię</th>
+                <th scope="col">Nazwisko</th>
+                <th scope="col">Numer telefonu</th>
+                <th scope="col">Akcje</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->surname }}</td>
+                        <td>{{ $user->phone_number }}</td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $users->links() }}
     </div>
 @endsection
