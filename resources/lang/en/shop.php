2d62fffcf5be06a97d66818ff5f82c271c8d2af1
elmaccho <106253529+elmaccho@users.noreply.github.com>
2023-11-05 22:06:35 +0100

    Tłumaczenie - en

---
+++ resources/lang/en/shop.php
@@ -0,0 +1,41 @@
+<?php
+    return [
+
+        'welcome' => [
+            'products' => 'Products',
+            'categories' => 'Categories',
+            'price' => 'Price',
+            'filter' => 'Filter',
+        ],
+        'columns' => [
+            'actions' => 'Actions'
+        ],
+        'messages' => [
+
+            'delete_title' => "Are you sure you want to delete the record?",
+            'delete_more_info' => "You won't be able to undo this decision!",
+            'delete_agree' => 'Yes',
+            'delete_cancel' => 'Cancel'
+        ],
+        'button' => [
+            'save' => 'Save',
+            'add' => 'Add'
+        ],
+        'product' => [
+            'add_title' => 'Add product',
+            'edit_title' => 'Edit product',
+            'show_title' => 'Show product',
+            'index_title' => 'List products',
+            'fields' => [
+                'title' => 'Add product',
+                'name' => 'Name',
+                'description' => 'Description',
+                'amount' => 'Amount',
+                'price' => 'Price',
+                'image' => 'Image',
+                'action' => 'Actions',
+                'category' => 'Category'
+            ]
+        ]
+    ]
+?>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-
 </head>
 <body>
     <div id="app">
